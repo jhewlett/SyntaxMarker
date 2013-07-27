182d4e5709829c53541c277c48d518712baa845f
Justin Hewlett <hewlett.jus@gmail.com>
2013-07-26 23:40:18 -0600

    More progress on c# marker - modified colors and add more keywords

--- src/markers/cSharpMarker.js
+++ src/markers/cSharpMarker.js
@@ -1,18 +1,17 @@
 SyntaxMarker.addLanguage('csharp', {
     root: [
-        [/\b(public|private|protected|abstract|internal|interface|class|const|readonly|struct|namespace|using|get|set|throw|new|void|foreach|for|while|do|in|true|false|var|return|null|extern|ref|out|static)\b/, 'keyword'],
-        [/\b(int|uint|long|short|bool|double|float|decimal|string)\b/, 'types'],
+        [/\b(public|private|protected|abstract|internal|interface|class|const|readonly|struct|namespace|using|get|set|throw|new|void|foreach|for|while|do|in|true|false|var|return|null|extern|ref|out|static|event|delegate|try|catch|finally|case|break|continue|goto)\b/, 'keyword'],
+        [/\b(int|uint|long|short|bool|double|float|decimal|string|object|if|else|switch|typeof|default|is|as)\b/, 'types'],
         //[/\b(DateTime|List|Array)\b/, 'types'],
         [/[0-9]+/, 'number'],
         [/[0-9]+\.[0-9]*/, 'number'],
         [/[0-9]*\.[0-9]+/, 'number'],
         [/"[^"]*"/, 'string'],
         [/'[^']*'/, 'symbol'],
-        [/\/\/\/.*/, 'doc-comment'],
+        [/\/\/\/.*/, 'comment'],
         [/\/\/.*/, 'comment'],
-        [/\/\*(.|\n)*?\*\//, 'multi-comment']
+        [/\/\*(.|\n)*?\*\//, 'comment']
         //scientific notation
         //string/id literals
-        //attributes (class, method)
     ]
 })
