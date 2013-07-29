c015aa686ef535cfc27624e11b97ebe580969910
Justin Hewlett <hewlett.jus@gmail.com>
2013-07-28 19:28:35 -0600

    added more c# keywords.

--- src/markers/cSharpMarker.js
+++ src/markers/cSharpMarker.js
@@ -1,7 +1,7 @@
 SyntaxMarker.addLanguage('csharp', {
     root: [
-        [/\b(public|private|protected|abstract|internal|interface|class|const|readonly|struct|namespace|using|get|set|throw|new|void|foreach|for|while|do|in|true|false|var|return|null|extern|ref|out|static|event|delegate|try|catch|finally|case|break|continue|goto)\b/, 'keyword'],
-        [/\b(int|uint|long|short|bool|double|float|decimal|string|object|if|else|switch|typeof|default|is|as)\b/, 'types'],
+        [/\b(enum|lock|base|sizeof|public|private|protected|abstract|internal|interface|class|const|readonly|struct|namespace|using|get|set|throw|new|void|foreach|for|while|do|in|true|false|var|return|null|extern|ref|out|static|event|delegate|try|catch|finally|case|break|continue|goto|virtual|override|sealed|operator|params|this|typeof|unsafe|checked|unchecked|explicit|implicit|fixed|volatile|if|else|switch|typeof|default|is|as|yield|await|dynamic|global|async)\b/, 'keyword'],
+        [/\b(byte|sbyte|ulong|ushort|char|int|uint|long|short|bool|double|float|decimal|string|object)\b/, 'types'],
         //[/\b(DateTime|List|Array)\b/, 'types'],
         [/[0-9]+/, 'number'],
         [/[0-9]+\.[0-9]*/, 'number'],
